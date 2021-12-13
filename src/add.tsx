21b18299a8006481b2967f597f78cd95ccef26f6
airvisualstudio <rianvputra@airvisualcreative.onmicsrosoft.com>
2021-12-13 20:57:36 +0700

    Kode tambahan ke 2 untuk add produk

---
+++ src/add.tsx
@@ -0,0 +1,30 @@
+function addNumbers(a: number, b: number) { 
+    return a + b; 
+} 
+
+var sum: number = addNumbers(10, 15) 
+
+console.log('Sum of the two numbers is: ' +sum); 
+
+let num1:number = 1; 
+    
+function letDeclaration() { 
+    let num2:number = 2; 
+
+    if (num2 > num1) { 
+        let num3: number = 3;
+        num3++; 
+    } 
+
+    while(num1 < num2) { 
+        let num4: number = 4;
+        num1++;
+    }
+
+    console.log(num1); //OK
+    console.log(num2); //OK 
+    console.log(num3); //Compiler Error: Cannot find name 'num3'
+    console.log(num4); //Compiler Error: Cannot find name 'num4'
+}
+
+letDeclaration();
