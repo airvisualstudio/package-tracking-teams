3de7645ed6274d4024485866f800f72eff982137
airvisualstudio <rianvputra@airvisualcreative.onmicsrosoft.com>
2022-01-12 11:10:43 +0700

    Bug Fixes 3

--- src/add.tsx
+++ src/add.tsx
@@ -28,3 +28,24 @@ function letDeclaration() {
 }
 
 letDeclaration();
+
+function printLabel(labeledObj: { label: string }) {
+    console.log(labeledObj.label);
+  }
+  
+  let myObj = { size: 10, label: "Size 10 Object" };
+  printLabel(myObj);
+  
+  interface LabeledValue {
+    label: string;
+  }
+   
+  function printLabel(labeledObj: LabeledValue) {
+    console.log(labeledObj.label);
+  }
+   
+  let myObj = { size: 10, label: "Size 10 Object" };
+  printLabel(myObj);
+
+
+
