0553be437288dab83edb5f2838c3674169da0a82
Airvisual Creative <rianvputra@airvisualcreative.onmicrosoft.com>
2023-02-23 12:49:19 +0700

    FebFix

--- src/TrackingItems.js
+++ src/TrackingItems.js
@@ -33,6 +33,27 @@ function App() {
   );
 }
 
+
+function Track() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
